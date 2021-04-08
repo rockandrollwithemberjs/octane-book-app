c3216467642c60ccd5d9640e60a67b861c2da623
Balint Erdi <balint@balinterdi.com>
2021-04-08 16:01:54 +0200

    Work against a real, JSON:API back-end

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -1,8 +1,6 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { dasherize } from '@ember/string';
 import { tracked } from '@glimmer/tracking';
-import Band from 'rarwe/models/band';
 import { inject as service } from '@ember/service';
 
 export default class BandsNewController extends Controller {
@@ -17,9 +15,8 @@ export default class BandsNewController extends Controller {
   }
 
   @action
-  saveBand() {
-    let band = new Band({ name: this.name, id: dasherize(this.name) });
-    this.catalog.add('band', band);
+  async saveBand() {
+    let band = await this.catalog.create('band', { name: this.name });
     this.router.transitionTo('bands.band.songs', band.id);
   }
 }
