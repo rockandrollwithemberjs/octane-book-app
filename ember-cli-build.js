49db6fbdc958194d2872c909e466eb1d4af54170
Balint Erdi <balint@balinterdi.com>
2021-05-06 11:00:45 +0200

    Add all but splitAtRoutes Embroider options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,5 +26,11 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return require('@embroider/compat').compatBuild(app, Webpack);
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticComponents: true,
+    // splitAtRoutes: ['route.name'], // can also be a RegExp
+  });
 };
