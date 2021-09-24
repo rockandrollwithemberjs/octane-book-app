1b714d68a25e61eee43cc5274b96ead4fb7583e5
Balint Erdi <balint@balinterdi.com>
2021-09-24 13:07:38 +0200

    Work against a real, JSON:API back-end

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,6 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import Song from 'rarwe/models/song';
 import { inject as service } from '@ember/service';
 
 export default class BandsBandSongsController extends Controller {
@@ -11,14 +10,23 @@ export default class BandsBandSongsController extends Controller {
   @service catalog;
 
   @action
+  async updateRating(song, rating) {
+    song.rating = rating;
+    this.catalog.update('song', song, { rating });
+  }
+
+  @action
   updateTitle(event) {
     this.title = event.target.value;
   }
 
   @action
-  saveSong() {
-    let song = new Song({ title: this.title, band: this.model });
-    this.catalog.add('song', song);
+  async saveSong() {
+    let song = await this.catalog.create(
+      'song',
+      { title: this.title },
+      { band: { data: { id: this.model.id, type: 'bands' } } }
+    );
     this.model.songs = [...this.model.songs, song];
     this.title = '';
     this.showAddSong = true;
--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,64 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import Band from 'rarwe/models/band';
-import Song from 'rarwe/models/song';
 
 export default class BandsRoute extends Route {
   @service catalog;
 
   model() {
-    let blackDog = new Song({
-      title: 'Black Dog',
-      rating: 3,
-    });
-
-    let yellowLedbetter = new Song({
-      title: 'Yellow Ledbetter',
-      rating: 4,
-    });
-
-    let pretender = new Song({
-      title: 'The Pretender',
-      rating: 2,
-    });
-
-    let daughter = new Song({
-      title: 'Daughter',
-      rating: 5,
-    });
-
-    let ledZeppelin = new Band({
-      id: 'led-zeppelin',
-      name: 'Led Zeppelin',
-      songs: [blackDog],
-    });
-
-    let pearlJam = new Band({
-      id: 'pearl-jam',
-      name: 'Pearl Jam',
-      songs: [yellowLedbetter, daughter],
-    });
-
-    let fooFighters = new Band({
-      id: 'foo-fighters',
-      name: 'Foo Fighters',
-      songs: [pretender],
-    });
-
-    blackDog.band = ledZeppelin;
-    yellowLedbetter.band = pearlJam;
-    daughter.band = pearlJam;
-    pretender.band = fooFighters;
-
-    this.catalog.add('song', blackDog);
-    this.catalog.add('song', yellowLedbetter);
-    this.catalog.add('song', daughter);
-    this.catalog.add('song', pretender);
-
-    this.catalog.add('band', ledZeppelin);
-    this.catalog.add('band', pearlJam);
-    this.catalog.add('band', fooFighters);
-
-    return this.catalog.bands;
+    return this.catalog.fetchAll('bands');
   }
 }
