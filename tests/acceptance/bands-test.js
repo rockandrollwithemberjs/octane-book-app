7bd8d1a6c9f2654a92b9db984ac5489a5f49b7e2
Balint Erdi <balint@balinterdi.com>
2021-10-09 22:13:02 +0200

    Test various aspects of the app

---
+++ tests/acceptance/bands-test.js
@@ -0,0 +1,50 @@
+import { module, test } from 'qunit';
+import { visit, waitFor } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { getPageTitle } from 'ember-page-title/test-support';
+import { createBand } from 'rarwe/tests/helpers/custom-helpers';
+
+module('Acceptance | bands', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('List bands', async function (assert) {
+    this.server.create('band', { name: 'Radiohead' });
+    this.server.create('band', { name: 'Long Distance Calling' });
+
+    await visit('/');
+    assert.equal(getPageTitle(), 'Bands | Rock & Roll with Octane');
+
+    assert
+      .dom('[data-test-rr="band-link"]')
+      .exists({ count: 2 }, 'All band links are rendered');
+    assert
+      .dom('[data-test-rr="band-list-item"]:first-child')
+      .hasText('Radiohead', 'The first band link contains the band name');
+    assert
+      .dom('[data-test-rr="band-list-item"]:last-child')
+      .hasText(
+        'Long Distance Calling',
+        'The other band link contains the band name'
+      );
+  });
+
+  test('Create a band', async function (assert) {
+    this.server.create('band', { name: 'Royal Blood' });
+
+    await visit('/');
+    await createBand('Caspian');
+    await waitFor('[data-test-rr="no-songs-text"]');
+
+    assert
+      .dom('[data-test-rr="band-list-item"]')
+      .exists({ count: 2 }, 'A new band link is rendered');
+    assert
+      .dom('[data-test-rr="band-list-item"]:last-child')
+      .hasText('Caspian', 'The new band link is rendered as the last item');
+    assert
+      .dom('[data-test-rr="songs-nav-item"] > .active')
+      .exists('The Songs tab is active');
+  });
+});
---
+++ tests/unit/services/catalog-test.js
@@ -0,0 +1,53 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import Band from 'rarwe/models/band';
+import Song from 'rarwe/models/song';
+
+module('Unit | Service | catalog', function (hooks) {
+  setupTest(hooks);
+
+  test('it can store and retrieve bands', function (assert) {
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.add('band', new Band({ id: 1, name: 'Led Zeppelin' }));
+
+    assert.equal(catalog.bands.length, 1);
+    assert.equal(catalog.bands[0].name, 'Led Zeppelin');
+  });
+
+  test('it can store and retrieve songs', function (assert) {
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.add(
+      'song',
+      new Song({
+        id: 1,
+        title: 'Achilles Last Stand',
+        rating: 5,
+      })
+    );
+    assert.equal(catalog.songs.length, 1);
+    assert.equal(catalog.songs[0].title, 'Achilles Last Stand');
+  });
+
+  test('it can load a record from a JSON:API response', function (assert) {
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.load({
+      data: {
+        type: 'bands',
+        id: '1',
+        attributes: {
+          name: 'TOOL',
+        },
+        relationships: {
+          songs: {
+            links: {
+              related: '/bands/1/songs',
+            },
+          },
+        },
+      },
+    });
+    let band = catalog.bands[0];
+    assert.equal(band.name, 'TOOL');
+    assert.equal(band.relationships.songs, '/bands/1/songs');
+  });
+});
