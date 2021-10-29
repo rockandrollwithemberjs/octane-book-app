e85e06554347a7acb6c8de066a252424380b3265
Balint Erdi <balint@balinterdi.com>
2021-10-29 14:33:09 +0200

    Create a form to add image to band

--- app/models/band.js
+++ app/models/band.js
@@ -4,9 +4,10 @@ export default class Band {
   @tracked name;
   @tracked songs;
 
-  constructor({ id, name, songs, description }, relationships = {}) {
+  constructor({ id, name, songs, imageURL, description }, relationships = {}) {
     this.id = id;
     this.name = name;
+    this.imageURL = imageURL;
     this.songs = songs || [];
     this.relationships = relationships;
     this.description = description;
