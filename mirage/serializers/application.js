ef827fe5be9f6a7be4bd4378d634f5e9f01ca152
Balint Erdi <balint@balinterdi.com>
2022-10-07 12:33:23 +0200

    Test various aspects of the app

---
+++ mirage/serializers/application.js
@@ -0,0 +1,15 @@
+import { JSONAPISerializer } from 'miragejs';
+
+export default JSONAPISerializer.extend({
+  links(resource) {
+    let { id, modelName } = resource;
+    if (modelName === 'band') {
+      return {
+        songs: {
+          related: `/bands/${id}/songs`,
+          self: `/bands/${id}/relationships/songs`,
+        },
+      };
+    }
+  },
+});
