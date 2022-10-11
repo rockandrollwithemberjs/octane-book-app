741e6b4a1fe85b37bd138ddcc867f86128070dc3
Balint Erdi <balint@balinterdi.com>
2022-10-11 09:53:32 +0200

    No need for Ember's capitalize

--- app/helpers/capitalize.js
+++ app/helpers/capitalize.js
@@ -1,9 +1,8 @@
 import { helper } from '@ember/component/helper';
-import { capitalize as emberCapitalize } from '@ember/string';
 
 export function capitalize(input) {
   let words = input[0].split(/\s+/).map((word) => {
-    return emberCapitalize(word.charAt(0)) + word.slice(1);
+    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
   });
   return words.join(' ');
 }
