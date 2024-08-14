c53c4b902959c39d5f70aeac1e0896574835264d
Balint Erdi <balint@balinterdi.com>
2024-08-14 20:13:05 +0200

    Work against a real, JSON:API back-end

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,6 +1,15 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class BandsBandSongsRoute extends Route {
+  @service catalog;
+
+  async model() {
+    let band = this.modelFor('bands.band');
+    await this.catalog.fetchRelated(band, 'songs');
+    return band;
+  }
+
   resetController(controller) {
     controller.title = '';
     controller.showAddSong = true;
