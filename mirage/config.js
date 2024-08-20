b3ecbab390195c3507e620c0636b321b2d0ddb84
Balint Erdi <balint@balinterdi.com>
2024-08-20 12:19:54 +0200

    Test various aspects of the app

---
+++ mirage/config.js
@@ -0,0 +1,22 @@
+import { createServer } from 'miragejs';
+
+export default function (config) {
+  let finalConfig = {
+    ...config,
+    models: config.models,
+    routes,
+  };
+
+  return createServer(finalConfig);
+}
+
+function routes() {
+  this.get('/bands');
+  this.get('/bands/:id');
+  this.get('/bands/:id/songs', function (schema, request) {
+    let id = request.params.id;
+    return schema.songs.where({ bandId: id });
+  });
+
+  this.post('/bands');
+}
---
+++ tests/unit/services/catalog-test.js
@@ -0,0 +1,53 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'rarwe/tests/helpers';
+import Band from 'rarwe/models/band';
+import Song from 'rarwe/models/song';
+
+module('Unit | Service | catalog', function (hooks) {
+  setupTest(hooks);
+
+  test('it can store and retrieve bands', function (assert) {
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.add('band', new Band({ id: 1, name: 'Led Zeppelin' }));
+
+    assert.strictEqual(catalog.bands.length, 1);
+    assert.strictEqual(catalog.bands[0].name, 'Led Zeppelin');
+  });
+
+  test('it can store and retrieve songs', function (assert) {
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.add(
+      'song',
+      new Song({
+        id: 1,
+        title: 'Achilles Last Stand',
+        rating: 5,
+      })
+    );
+    assert.strictEqual(catalog.songs.length, 1);
+    assert.strictEqual(catalog.songs[0].title, 'Achilles Last Stand');
+  });
+
+  test('it can load a record from a JSON:API response', function (assert) {
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.load({
+      data: {
+        type: 'bands',
+        id: '1',
+        attributes: {
+          name: 'TOOL',
+        },
+        relationships: {
+          songs: {
+            links: {
+              related: '/bands/1/songs',
+            },
+          },
+        },
+      },
+    });
+    let band = catalog.bands[0];
+    assert.strictEqual(band.name, 'TOOL');
+    assert.strictEqual(band.relationships.songs, '/bands/1/songs');
+  });
+});
