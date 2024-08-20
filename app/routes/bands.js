647885b5ad3c8e514dd9cbb477a52bd2cc339fcd
Balint Erdi <balint@balinterdi.com>
2024-08-20 12:24:05 +0200

    Render loading and error templates

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -4,7 +4,7 @@ import { service } from '@ember/service';
 export default class BandsRoute extends Route {
   @service catalog;
 
-  model() {
+  async model() {
     return this.catalog.fetchAll('bands');
   }
 }
